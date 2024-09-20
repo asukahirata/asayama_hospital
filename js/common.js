24396a9ca84fdca2d6d5dc83b4f55d924601f4b9
平田明日香 <a.hirata@cae-sar.co.jp>
2024-09-20 12:02:37 +0900

    スライダー追加一旦コミット

--- js/common.js
+++ js/common.js
@@ -35,3 +35,27 @@ $('a[href^="#"]').click(function() {
   $('body,html').animate({scrollTop:position}, speed, 'swing');
   return false;
 });
+
+
+//スライダー
+var $slide = $(".slide")
+  .slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    autoplay: true
+  })
+  .on({
+    beforeChange: function(event, slick, currentSlide, nextSlide) {
+      $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
+      $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
+    },
+    afterChange: function() {
+      $(".preve-slide", this).removeClass("preve-slide slide-animation");
+    }
+  });
+$slide.find(".slick-slide").eq(0).addClass("slide-animation");
